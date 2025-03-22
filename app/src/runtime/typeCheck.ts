96d26d7e2ae66174f808be77417cb07d2e80447f
Robin Harbecke <robin.harbecke@gmail.com>
2025-03-22 16:44:09 +0100

    Update type check

--- app/src/runtime/typeCheck.ts
+++ app/src/runtime/typeCheck.ts
@@ -4,12 +4,19 @@ export const WebScadModuleSchema = z.object({
     main: z.function(),
 });
 
+export const WebScadColorValue = z.number().min(0).max(1);
+export const WebScadColor = z.union([
+    z.tuple([WebScadColorValue, WebScadColorValue, WebScadColorValue]),
+    z.tuple([WebScadColorValue, WebScadColorValue, WebScadColorValue, WebScadColorValue]),
+]);
+
 export const WebScadObjectSchema = z.object({
     type: z.literal("mesh"),
     vertices: z.instanceof(Float32Array),
     indices: z.instanceof(Uint32Array),
-    //TODO Color
+    color: WebScadColor.optional(),
 });
+
 
 export const WebScadResultSchema = z.union([//Union means or
     WebScadObjectSchema,//TODO  
