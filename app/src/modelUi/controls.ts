2c6a7b3660247d9f23e7bc2c491ffc4b7d25e887
Robin Harbecke <robin.harbecke@gmail.com>
2025-04-08 22:42:29 +0200

    Display status and error

--- app/src/modelUi/controls.ts
+++ app/src/modelUi/controls.ts
@@ -2,10 +2,24 @@ import { export3mf } from "../export/3mf";
 import { downloadBlob } from "../export/download";
 import { ResultRenderer } from "../rendering";
 import { JsRunner } from "../runtime/runner";
-import { ModelStateOk } from "../state";
-import { cButton, cDiv } from "../utils/jsml";
+import { ModelState, ModelStateCompilationFailed, ModelStateExecutionFailed, ModelStateOk } from "../state";
+import { cButton, cDiv, uElement } from "../utils/jsml";
 
 export class ModelUiControls {
+	private readonly errorDisplay = uElement<HTMLTextAreaElement>(document.createElement("textarea"), {
+		class: "error",
+		custom: [
+			v => v.readOnly = true,
+		],
+	});
+
+	private readonly status = cDiv({
+		class: ["status", "isWorking"],
+		children: [
+			cDiv({ class: "spinner" }),
+		],
+	});
+
 	public readonly container = cDiv({
 		class: "controls",
 		children: [
@@ -15,6 +29,8 @@ export class ModelUiControls {
 					cButton({ text: "Export 3mf", onClickHandled: () => this.export() }),
 				],
 			}),
+			this.errorDisplay,
+			this.status,
 		],
 	});
 
@@ -22,6 +38,8 @@ export class ModelUiControls {
 		private readonly runner: JsRunner,
 		private readonly renderer: ResultRenderer,
 	) {
+		this.runner.currentState.valueChanged.addListener(state => this.update(state));
+		this.runner.isWorking.valueChanged.addListener(isWorking => this.status.classList.toggle("isWorking", isWorking));
 	}
 
 	private async export() {
@@ -33,6 +51,19 @@ export class ModelUiControls {
 				const result = await export3mf(model, this.renderer.canvas);
 				downloadBlob(result, "my.3mf");
 			}
+		}
+	}
+
+	private update(state: ModelState) {
+		if (state instanceof ModelStateCompilationFailed) {
+			this.errorDisplay.classList.add("hasError");
+			this.errorDisplay.textContent = "Build Failed: \n" + String(state.error);
+		} else if (state instanceof ModelStateExecutionFailed) {
+			this.errorDisplay.classList.add("hasError");
+			this.errorDisplay.textContent = "Execution Failed: \n" + String(state.error);
+		} else {
+			this.errorDisplay.textContent = "";
+			this.errorDisplay.classList.remove("hasError");
 		}
 	}
 }
