0852055207db88912d848199ee8c839d455181f7
Robin Harbecke <robin.harbecke@gmail.com>
2025-04-23 17:49:50 +0200

    Update pipe impl

--- packages/web-scad-manifold-lib/pipe.ts
+++ packages/web-scad-manifold-lib/pipe.ts
@@ -7,9 +7,9 @@ interface Pipe {
 	<T1, T2, T3, T4>(v: T1, f1: Fn<T1, T2>, f2: Fn<T2, T3>, f3: Fn<T3, T4>): T4,
 	<T1, T2, T3, T4, T5>(v: T1, f1: Fn<T1, T2>, f2: Fn<T2, T3>, f3: Fn<T3, T4>, f4: Fn<T4, T5>): T5,
 	<T1, T2, T3, T4, T5, T6>(v: T1, f1: Fn<T1, T2>, f2: Fn<T2, T3>, f3: Fn<T3, T4>, f4: Fn<T4, T5>, f5: Fn<T5, T6>): T6,
+	<T>(v: T, ...fns: readonly Fn<T, T>[]): T,
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-function-type
-export const pipe: Pipe = (v: any, ...fns: Function[]): unknown => {
+export const pipe: Pipe = <T>(v: T, ...fns: readonly Fn<T, T>[]): T => {
 	return fns.reduce((acc, fn) => fn(acc), v);
 };
