948149d091c73e6e8ce58b55c2371ec5b8cbb544
Robin Harbecke <robin.harbecke@gmail.com>
2025-06-07 00:06:22 +0200

    Add text2d rendering

--- packages/web-scad-manifold-lib/2d/object2d.ts
+++ packages/web-scad-manifold-lib/2d/object2d.ts
@@ -1,6 +1,7 @@
 import { Rect } from "manifold-3d";
 import { CrossSectionGc } from "../internal";
 import { CrossSection } from "../internal/bindings";
+import { SimpleVec2 } from "./vec2";
 
 export class Object2D {
 	public constructor(
@@ -8,6 +9,11 @@ export class Object2D {
 	) { }
 
 	public static fromCrossSection = (crossSection: CrossSection) => new Object2D(new CrossSectionGc(crossSection));
+
+	public static fromPolygons(polygons: readonly SimpleVec2[] | readonly SimpleVec2[][], fillRule?: "EvenOdd" | "NonZero" | "Positive" | "Negative"): Object2D {
+		const crossSection = new CrossSection(polygons as [number, number][] | [number, number][][], fillRule);
+		return Object2D.fromCrossSection(crossSection);
+	}
 
 	public apply = (operation: (input: CrossSection) => CrossSection): Object2D => {
 		return Object2D.fromCrossSection(operation(this.crossSection.internal));
---
+++ packages/web-scad-manifold-lib/2d/text2d.ts
@@ -0,0 +1,87 @@
+import opentype, { PathCommand } from "opentype.js";
+import { SimpleVec2 } from "./vec2";
+import { Object2D } from "./object2d";
+
+export const text2d = (text: string, fontData: ArrayBuffer, fontSize: number, options: Text2dOptions = {}): Object2D => {
+	const font = opentype.parse(fontData);
+	const path = font.getPath(text, 0, 0, fontSize, {
+		kerning: options.kerning,
+	});
+
+	const polygon = commandsToPolygon(path.commands, options.curveSplits ?? 1);
+
+	return Object2D.fromPolygons(polygon);
+};
+
+const commandsToPolygon = (commands: readonly PathCommand[], curveSplits: number) => {
+	const result: SimpleVec2[][] = [];
+
+	let startPosition: SimpleVec2 | undefined;
+	let lastKnownPosition: SimpleVec2 | undefined;
+	let currentPath: SimpleVec2[] = [];
+	for (const command of commands) {
+		switch (command.type) {
+			case "M":
+				{
+					const pos: [number, number] = [command.x, command.y];
+					lastKnownPosition = pos;
+					startPosition = pos;
+					currentPath.push(pos);
+				}
+				break;
+			case "L":
+				{
+					const pos: [number, number] = [command.x, command.y];
+					lastKnownPosition = pos;
+					currentPath.push(pos);
+				}
+				break;
+			case "Z":
+				lastKnownPosition = startPosition;
+				if (startPosition === undefined) throw new Error("Can't convert text to path. The start position is not known.");
+				currentPath.push(startPosition!);
+				result.push(currentPath);
+				currentPath = [];
+				break;
+			case "Q":
+				{
+					const pos: [number, number] = [command.x, command.y];
+					if (lastKnownPosition === undefined) throw new Error("Can't convert text to path. The last position is not known.");
+					currentPath.push(...cubicBezierCurveToPoints(lastKnownPosition, [command.x1, command.y1], pos, curveSplits));
+					lastKnownPosition = pos;
+				}
+				break;
+			default:
+				throw new Error(`Unknown path command ${command.type}`);
+		}
+	}
+	return result;
+};
+
+const cubicBezierCurveToPoints = (start: SimpleVec2, control: SimpleVec2, end: SimpleVec2, splits: number) => {
+	const intermediatePoints: SimpleVec2[] = [];
+	const divider = splits + 1;
+	for (let i = 0; i < splits; i++) {
+		const t = (i + 1) / divider;
+		intermediatePoints.push(cubicBezierCurve(start, control, end, t));
+	}
+	return [
+		start,
+		...intermediatePoints,
+		end,
+	];
+};
+
+const cubicBezierCurve = (start: SimpleVec2, control: SimpleVec2, end: SimpleVec2, t: number): SimpleVec2 => {
+	const tSquared = t ** 2;
+
+	return [
+		(start[0] - 2 * control[0] + end[0]) * tSquared + (-2 * start[0] + 2 * control[0]) * t + start[0],
+		(start[1] - 2 * control[1] + end[1]) * tSquared + (-2 * start[1] + 2 * control[1]) * t + start[1],
+	];
+};
+
+export interface Text2dOptions {
+	readonly curveSplits?: number,
+	readonly kerning?: boolean,
+}
