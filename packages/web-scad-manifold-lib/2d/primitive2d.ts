0903b938dcc73f8f9c4a9be54860dd537c141c86
Robin Harbecke <robin.harbecke@gmail.com>
2025-06-27 20:53:45 +0200

    Add explicit default values

--- packages/web-scad-manifold-lib/2d/primitive2d.ts
+++ packages/web-scad-manifold-lib/2d/primitive2d.ts
@@ -2,5 +2,5 @@ import { CrossSection } from "../internal/bindings";
 import { Object2D } from "./object2d";
 import { AnyVec2, asSimpleVec2 } from "./vec2";
 
-export const square = (size?: AnyVec2, center?: boolean) => Object2D.fromCrossSection(CrossSection.square(size !== undefined ? asSimpleVec2(size) : undefined, center));
+export const square = (size?: AnyVec2, center?: boolean) => Object2D.fromCrossSection(CrossSection.square(asSimpleVec2(size ?? [1, 1]), center));
 export const circle = (radius: number, circularSegments?: number) => Object2D.fromCrossSection(CrossSection.circle(radius, circularSegments));
--- packages/web-scad-manifold-lib/3d/primitive3d.ts
+++ packages/web-scad-manifold-lib/3d/primitive3d.ts
@@ -2,6 +2,6 @@ import { Manifold } from "../internal/bindings";
 import { Object3D } from "./object3d";
 import { AnyVec3, asSimpleVec3 } from "./vec3";
 
-export const cube = (size?: AnyVec3, center?: boolean) => Object3D.fromManifold(Manifold.cube(size !== undefined ? asSimpleVec3(size) : undefined, center));
-export const sphere = (radius: number, circularSegments?: number) => Object3D.fromManifold(Manifold.sphere(radius, circularSegments));
+export const cube = (size?: AnyVec3, center?: boolean) => Object3D.fromManifold(Manifold.cube(asSimpleVec3(size ?? [1, 1, 1]), center));
+export const sphere = (radius: number, circularSegments?: number) => Object3D.fromManifold(Manifold.sphere(radius ?? 1, circularSegments));
 export const tetrahedron = () => Object3D.fromManifold(Manifold.tetrahedron());
