714046b497e50b56b4ed9f5b149e164143ac0037
Robin Harbecke <robin.harbecke@gmail.com>
2025-07-03 22:44:16 +0200

    Simplify circle

--- packages/web-scad-manifold-lib/2d/primitive2d.ts
+++ packages/web-scad-manifold-lib/2d/primitive2d.ts
@@ -3,4 +3,4 @@ import { Object2D } from "./object2d";
 import { AnyVec2, asSimpleVec2 } from "./vec2";
 
 export const square = (size?: AnyVec2, center?: boolean) => Object2D.fromCrossSection(CrossSection.square(asSimpleVec2(size ?? [1, 1]), center));
-export const circle = (radius: number, circularSegments?: number) => Object2D.fromCrossSection(CrossSection.circle(radius, circularSegments));
+export const circle = (radius = 1, circularSegments?: number) => Object2D.fromCrossSection(CrossSection.circle(radius, circularSegments));
