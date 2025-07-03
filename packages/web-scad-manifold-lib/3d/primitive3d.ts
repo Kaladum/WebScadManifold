c6429ddcfacc721bbd740d7c360179c354cfd661
Robin Harbecke <robin.harbecke@gmail.com>
2025-07-03 22:43:23 +0200

    Add cylinder

--- packages/web-scad-manifold-lib/3d/primitive3d.ts
+++ packages/web-scad-manifold-lib/3d/primitive3d.ts
@@ -1,7 +1,11 @@
+import { circle, extrude2d } from "../2d";
 import { Manifold } from "../internal/bindings";
+import { pipe } from "../pipe";
 import { Object3D } from "./object3d";
 import { AnyVec3, asSimpleVec3 } from "./vec3";
 
 export const cube = (size?: AnyVec3, center?: boolean) => Object3D.fromManifold(Manifold.cube(asSimpleVec3(size ?? [1, 1, 1]), center));
 export const sphere = (radius: number, circularSegments?: number) => Object3D.fromManifold(Manifold.sphere(radius ?? 1, circularSegments));
 export const tetrahedron = () => Object3D.fromManifold(Manifold.tetrahedron());
+
+export const cylinder = (radius = 1, height = 1) => pipe(circle(radius), extrude2d(height));
