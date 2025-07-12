9f6d105f7958b552c32a33da9f94638d473b5714
Robin Harbecke <robin.harbecke@gmail.com>
2025-07-12 16:21:45 +0200

    Extend vector types

--- packages/web-scad-manifold-lib/2d/vec2.ts
+++ packages/web-scad-manifold-lib/2d/vec2.ts
@@ -5,13 +5,23 @@ export class Vec2 {
 		public readonly x: number,
 		public readonly y: number,
 	) { }
+
+	public readonly xy = (): [number, number] => [this.x, this.y];
+
+	public static from(vec: AnyVec2): Vec2 {
+		if (vec instanceof Vec2) {
+			return vec;
+		} else {
+			return new Vec2(...vec);
+		}
+	}
 }
 
 export type AnyVec2 = SimpleVec2 | Vec2;
 
 export function asSimpleVec2(vec2: AnyVec2): SimpleVec2 {
 	if (vec2 instanceof Vec2) {
-		return [vec2.x, vec2.y];
+		return vec2.xy();
 	} else {
 		return vec2;
 	}
--- packages/web-scad-manifold-lib/3d/vec3.ts
+++ packages/web-scad-manifold-lib/3d/vec3.ts
@@ -6,13 +6,23 @@ export class Vec3 {
 		public readonly y: number,
 		public readonly z: number,
 	) { }
+
+	public readonly xyz = (): [number, number, number] => [this.x, this.y, this.z];
+
+	public static from(vec: AnyVec3): Vec3 {
+		if (vec instanceof Vec3) {
+			return vec;
+		} else {
+			return new Vec3(...vec);
+		}
+	}
 }
 
 export type AnyVec3 = SimpleVec3 | Vec3;
 
 export function asSimpleVec3(vec3: AnyVec3): SimpleVec3 {
 	if (vec3 instanceof Vec3) {
-		return [vec3.x, vec3.y, vec3.z];
+		return vec3.xyz();
 	} else {
 		return vec3;
 	}
