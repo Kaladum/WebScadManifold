fb67d6cc6b2b680c73aa69eebb5bd2116ea6313f
Robin Harbecke <robin.harbecke@gmail.com>
2025-07-12 16:17:08 +0200

    Check color values on material creation

--- packages/web-scad-manifold-lib/material.ts
+++ packages/web-scad-manifold-lib/material.ts
@@ -3,6 +3,9 @@ export class Material {
 
 	public constructor(color?: readonly [number, number, number] | readonly [number, number, number, number]) {
 		if (color !== undefined) {
+			for (const element of color) {
+				if (element < 0 || element > 1) throw new Error("Color values must be between 0 and 1");
+			}
 			if (color.length === 4) {
 				this.color = color;
 			} else {
