4a482be083cd19390b956da9d4af4b4edbbfbad7
Robin Harbecke <robin.harbecke@gmail.com>
2025-10-07 20:01:50 +0200

    Add vector methods

--- packages/web-scad-manifold-lib/2d/vec2.ts
+++ packages/web-scad-manifold-lib/2d/vec2.ts
@@ -6,6 +6,8 @@ export class Vec2 {
 		public readonly y: number,
 	) { }
 
+	public readonly equals = (b: Vec2) => this.x === b.x && this.y === b.y;
+
 	public readonly xy = (): [number, number] => [this.x, this.y];
 
 	public static from(vec: AnyVec2): Vec2 {
@@ -15,6 +17,12 @@ export class Vec2 {
 			return new Vec2(...vec);
 		}
 	}
+
+	public readonly scale = (factor: number): Vec2 => new Vec2(this.x * factor, this.y * factor);
+
+	public readonly subtract = (other: Vec2): Vec2 => new Vec2(this.x - other.x, this.y - other.y);
+
+	public readonly dot = (other: Vec2): number => this.x * other.x + this.y * other.y;
 }
 
 export type AnyVec2 = SimpleVec2 | Vec2;
--- packages/web-scad-manifold-lib/3d/vec3.ts
+++ packages/web-scad-manifold-lib/3d/vec3.ts
@@ -9,6 +9,16 @@ export class Vec3 {
 
 	public readonly xyz = (): [number, number, number] => [this.x, this.y, this.z];
 
+	public equals(b: Vec3) {
+		return this.x === b.x && this.y === b.y && this.z === b.z;
+	}
+
+	public readonly scale = (factor: number): Vec3 => new Vec3(this.x * factor, this.y * factor, this.z * factor);
+
+	public readonly subtract = (other: Vec3): Vec3 => new Vec3(this.x - other.x, this.y - other.y, this.z - other.z);
+
+	public readonly dot = (other: Vec3): number => this.x * other.x + this.y * other.y + this.z * other.z;
+
 	public static from(vec: AnyVec3): Vec3 {
 		if (vec instanceof Vec3) {
 			return vec;
